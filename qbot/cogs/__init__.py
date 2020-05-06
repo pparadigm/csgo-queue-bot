2771d5b9dc46105d6efd34a8640aced096ff11cf
Arnold Leigh Ryan Choa <alrlchoa@gmail.com>
2020-05-06 15:36:06 +0800

    Update __init__.py

--- qbot/cogs/__init__.py
+++ qbot/cogs/__init__.py
@@ -3,7 +3,6 @@
 from .cacher import CacherCog
 from .console import ConsoleCog
 from .dbl import DblCog
-from .donate import DonateCog
 from .help import HelpCog
 from .mapdraft import MapDraftCog
 from .popflash import PopflashCog
